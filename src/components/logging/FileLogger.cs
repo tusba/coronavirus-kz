7f5eb2f67ad0ddef5714ba4d11001d523c50b72e
tusba <11007086+tusba@users.noreply.github.com>
2021-07-29 21:48:03 +0600

    add logging to file

--- src/components/logging/FileLogger.cs
+++ src/components/logging/FileLogger.cs
@@ -1,15 +1,55 @@
 using System;
+using System.IO;
 
 namespace Tusba.Components.Logging
 {
 	public class FileLogger : InterfaceLogger
 	{
-		public FileLogger(string filePath)
+		// project's root directory
+		protected readonly string baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../../..");
+		// relative path from this.baseDir
+		protected string dirPath = "";
+		protected string fileName;
+
+		private string fullFileName;
+
+		public FileLogger(string fileName)
 		{
+			this.fileName = fileName;
+			updateFilePath();
 		}
 
+		public string BaseDirectory => baseDir;
+
+		public string Directory
+		{
+			set
+			{
+				dirPath = value;
+				updateFilePath();
+			}
+		}
+
+		/**
+		 * @throws IOException
+		 */
 		public void Log(object target)
 		{
+			string logText = target.ToString() + Environment.NewLine;
+
+			if (File.Exists(fullFileName))
+			{
+				File.AppendAllText(fullFileName, logText);
+			}
+			else
+			{
+				File.WriteAllText(fullFileName, logText);
+			}
+		}
+
+		private void updateFilePath()
+		{
+			fullFileName = Path.Combine(baseDir, dirPath, fileName);
 		}
 	}
 }
--- test/FileLoggerTest.cs
+++ test/FileLoggerTest.cs
@@ -3,11 +3,17 @@ using Xunit;
 
 namespace test
 {
-    public class FileLoggerTest
-    {
-        [Fact]
-        public void LogTest()
-        {
-        }
-    }
+	public class FileLoggerTest
+	{
+		[Fact]
+		public void NonExistingDirectoryTest()
+		{
+			// todo catch System.IO.DirectoryNotFoundException
+		}
+
+		[Fact]
+		public void LogTest()
+		{
+		}
+	}
 }
