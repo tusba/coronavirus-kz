45f2a229f77076282d7b6b0390489986851bba89
tusba <11007086+tusba@users.noreply.github.com>
2021-08-03 21:40:53 +0600

    Add test for file logging into non-existent directory

--- test/FileLoggerTest.cs
+++ test/FileLoggerTest.cs
@@ -1,14 +1,30 @@
 using System;
+using System.IO;
 using Xunit;
+using Tusba.Components.Logging;
 
 namespace test
 {
 	public class FileLoggerTest
 	{
+		// project's root directory
+		protected readonly string baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../../..");
+
 		[Fact]
 		public void NonExistingDirectoryTest()
 		{
-			// todo catch System.IO.DirectoryNotFoundException
+			string logDir = @"log-test";
+			string logPath = Path.Combine(baseDir, logDir);
+
+			Assert.False(Directory.Exists(logPath));
+
+			Assert.Throws<DirectoryNotFoundException>(() => {
+				InterfaceLogger logger = new FileLogger("test-1.log");
+				((FileLogger) logger).Directory = logDir;
+				logger.Log("Some test data");
+			});
+
+			Assert.False(Directory.Exists(logPath));
 		}
 
 		[Fact]
