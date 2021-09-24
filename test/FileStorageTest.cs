d3da925c8a4c382a2668afa932a65285ecb20201
tusba <11007086+tusba@users.noreply.github.com>
2021-09-24 21:06:21 +0600

    Add test for file storage

---
+++ test/FileStorageTest.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using Xunit;
+using Tusba.Components.FileSystem;
+
+namespace test
+{
+	public class FileStorageTest
+	{
+		// project's root directory
+		private readonly string baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../../..");
+
+		[Fact]
+		public void ProvideNonExistingDirectoryTest()
+		{
+			string dirName = @"dir-test";
+			string dirPath = Path.Combine(baseDir, dirName);
+
+			Assert.False(Directory.Exists(dirPath));
+
+			for (int i = 0; i < 2; i++) {
+				Assert.True(FileStorage.ProvideDirectory(dirName));
+				Assert.True(Directory.Exists(dirPath));
+			}
+
+			Directory.Delete(dirPath);
+		}
+
+		[Fact]
+		public void ProvideExistingDirectoryTest()
+		{
+			string dirName = @"test";
+			string dirPath = Path.Combine(baseDir, dirName);
+
+			Assert.True(Directory.Exists(dirPath));
+
+			for (int i = 0; i < 2; i++) {
+				Assert.True(FileStorage.ProvideDirectory(dirName));
+				Assert.True(Directory.Exists(dirPath));
+			}
+		}
+	}
+}
