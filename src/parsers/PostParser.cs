e01b42ce8e7f8f0e71160c77fd3862a05e0c9f5d
tusba <11007086+tusba@users.noreply.github.com>
2021-11-04 21:04:43 +0600

    Extract post content from post page HTML

--- src/parsers/PostParser.cs
+++ src/parsers/PostParser.cs
@@ -1,16 +1,26 @@
+using System.Linq;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace Tusba.Components.Parsers
 {
-  public class PostParser : InterfacePostParser
-  {
-    public string RawContent { get; init; }
+	public class PostParser : InterfacePostParser
+	{
+		public string RawContent { get; init; }
 
-    public PostParser(string rawContent) => RawContent = rawContent;
+		public PostParser(string rawContent) => RawContent = rawContent;
 
-    public async Task<string[]> Parse()
-    {
-      return await Task.Run(() => new string[] {});
-    }
-  }
+		public async Task<string[]> Parse()
+		{
+			return await Task.Run(() => {
+				var htmlDoc = new HtmlDocument();
+				htmlDoc.LoadHtml(RawContent);
+
+				return htmlDoc.DocumentNode
+					.SelectNodes("//*[@data-post]//*[contains(@class,'tgme_widget_message_text')]")
+					.Select(node => node.InnerHtml)
+					.ToArray();
+			});
+		}
+	}
 }
