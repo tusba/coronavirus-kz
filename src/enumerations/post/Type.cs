cd390685ab98086ea8909f13f52869536bd3aba9
tusba <11007086+tusba@users.noreply.github.com>
2021-11-07 21:58:00 +0600

    Add post type detection

--- src/enumerations/post/Type.cs
+++ src/enumerations/post/Type.cs
@@ -1,25 +1,71 @@
+using System;
+
 namespace Tusba.Enumerations.Post
 {
-  public enum Type
-  {
-    // default type
-    COMMON,
+	public enum Type
+	{
+		// default type
+		COMMON,
 
-    // statistics on diseased people
-    STATS_DISEASED,
+		// statistics on diseased people
+		STATS_DISEASED,
 
-    // statistics on cases of pneumonia
-    STATS_PNEUMONIA,
+		// statistics on cases of pneumonia
+		STATS_PNEUMONIA,
 
-    // statistics on recovered people
-    STATS_RECOVERED
-  }
+		// statistics on recovered people
+		STATS_RECOVERED
+	}
 
-  public static class TypeExtensions
-  {
-    public static Type ResolvePostType(this string s) => s.ToLower() switch
-    {
-      _ => Type.COMMON,
-    };
-  }
+	public static class TypeExtensions
+	{
+		public static Type ResolvePostType(this string s)
+		{
+			var matcher = new PostMatcher(s);
+
+			if (matcher.MatchPeopleStats() && matcher.MatchPeopleRecoveredStats())
+			{
+				return Type.STATS_RECOVERED;
+			}
+
+			if (matcher.MatchPeopleStats() && matcher.MatchPeopleDiseasedStats())
+			{
+				return Type.STATS_DISEASED;
+			}
+
+			if (matcher.MatchPeoplePneumoniaStats())
+			{
+				return Type.STATS_PNEUMONIA;
+			}
+
+			return Type.COMMON;
+		}
+
+		private struct PostMatcher
+		{
+			private string Content { get; init; }
+
+			public PostMatcher(string content) => Content = content;
+
+			public bool MatchPeopleStats() => Content.Contains(
+				"За прошедшие сутки в Казахстане",
+				StringComparison.OrdinalIgnoreCase
+			);
+
+			public bool MatchPeopleRecoveredStats() => Content.Contains(
+				"выздорове",
+				StringComparison.OrdinalIgnoreCase
+			);
+
+			public bool MatchPeopleDiseasedStats() => Content.Contains(
+				"заболевши",
+				StringComparison.OrdinalIgnoreCase
+			);
+
+			public bool MatchPeoplePneumoniaStats() => Content.Contains(
+				"пневмонией с признаками коронавирусной инфекции",
+				StringComparison.OrdinalIgnoreCase
+			);
+		}
+	}
 }
